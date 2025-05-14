a5029f4c4c8f32e3a663280768f03a1d15709b7e
tkdltk9 <tkdltk9@naver.com>
2025-05-14 17:08:02 +0900

    파일 공유 및 기본 설정

---
+++ src/main/java/TripSync/TripSyncApplication.java
@@ -0,0 +1,22 @@
+package TripSync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@SpringBootApplication
+public class TripSyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripSyncApplication.class, args);
+	}
+	@RequestMapping("/")
+	public String index() {
+		return "thymeleaf/";
+	}
+	
+}
---
+++ src/test/java/TripSync/TripSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package TripSync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripSyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
